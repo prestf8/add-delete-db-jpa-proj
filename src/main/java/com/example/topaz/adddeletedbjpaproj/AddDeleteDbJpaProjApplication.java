a6ff30137863db804e1c0590a8890736522bb8f8
prestf8 <prestf8@gmail.com>
2021-04-03 00:33:53 -0700

    setup

---
+++ src/main/java/com/example/topaz/adddeletedbjpaproj/AddDeleteDbJpaProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.topaz.adddeletedbjpaproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddDeleteDbJpaProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddDeleteDbJpaProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/topaz/adddeletedbjpaproj/AddDeleteDbJpaProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.topaz.adddeletedbjpaproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddDeleteDbJpaProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
